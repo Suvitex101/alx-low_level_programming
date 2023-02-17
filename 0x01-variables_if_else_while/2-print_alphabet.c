8eeef481fce13e43afd9d152f34234008473e642
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-02-17 13:09:03 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int alph;
+for (alph = 'a'; alph <= 'z' ; alph++)
+putchar(alph);
+putchar('\n');
+return (0);
+}

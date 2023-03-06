2ea306a644a6b12d2d9de53ca0634a33ed0111f0
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-03-06 13:50:45 -0800

    c pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,9 +7,11 @@
  * @n: number of bytes
  * Return: return to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		*(dest + i) = *(src + i);

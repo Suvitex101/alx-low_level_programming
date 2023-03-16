83b030188701a010ce45a209e58ce5eaee78e563
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-03-16 15:48:08 -0700

    Advance

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

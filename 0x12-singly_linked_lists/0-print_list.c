c016b5952cb844911996b4199420ccc5cb5b9669
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-03-30 17:23:09 -0700

    singly link list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
-		
+
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		move++;
-		
+
 
 
 	}

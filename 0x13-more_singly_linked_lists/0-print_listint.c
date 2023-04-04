238746e898128f35ddfcf3643380420a575e9b23
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-04-04 16:06:27 -0700

    More singly link list functions

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
-
-	count = 0;
+	size_t new = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		new++;
 	}
 
-	return (count);
+	return (new);
 }

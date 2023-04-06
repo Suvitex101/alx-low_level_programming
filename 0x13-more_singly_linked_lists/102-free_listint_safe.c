fa66686bff96ad8247b29b667114bd4b80f5cb97
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-04-05 17:04:46 -0700

    More singly link list advance

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,6 +8,31 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	int ahead;
+	listint_t *nodeNew;
+	size_t count = 0;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		ahead = *h - (*h)->next;
+		if (ahead > 0)
+		{
+			nodeNew = (*h)->next;
+			*h = nodeNew;
+			count++;
+		}
+		else
+		{
+			*h = NULL;
+			count++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (count);
 }

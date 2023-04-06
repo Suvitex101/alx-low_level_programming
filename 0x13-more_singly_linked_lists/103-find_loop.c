86b0b8f80fe5b3dfb182170a4dc7d68210af6f1f
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-04-05 18:57:48 -0700

    More singly link list advance

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,7 +10,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *up, *down;
 
-	if (!head || head->next == NULL)
+	if (head == NULL || head->next == NULL)
 		return (NULL);
 
 	up = head->next;

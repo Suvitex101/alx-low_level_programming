328ca0eafea89e5dcd1e0acb92480701cd6c50ba
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-06-15 10:02:39 -0700

    Doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,15 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp;
-	size_t i = 0;
+	size_t b = 0;
 
 	temp = h;
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
-		i++;
+		b++;
 	}
-	return (i);
+	return (b);
 
 }
